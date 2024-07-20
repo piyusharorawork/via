fcc5917cf623b7b362e21e95562869a62728ae87
piyush arora <piyusharorawork@gmail.com>
2024-07-21 00:21:30 +0530

    remove components package

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -1,9 +1,7 @@
 import type { Config } from "tailwindcss";
+import sharedConfig from "@via/tailwind-config";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
+  presets: [sharedConfig],
+} satisfies Pick<Config, "prefix" | "presets" | "content">;
--- packages/components/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["src/**/*.{tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-} satisfies Config;
