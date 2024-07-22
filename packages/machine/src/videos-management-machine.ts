c5eb494029ff83ed93ba99e2f24327990f97ec7a
piyush arora <piyusharorawork@gmail.com>
2024-07-22 18:37:17 +0530

    use final states

--- packages/machine/src/videos-management-machine.ts
+++ packages/machine/src/videos-management-machine.ts
@@ -7,7 +7,7 @@ import { ListVideosOutput } from "@via/core/video-manager";
 export const getVideoManagmentMachine = (fetch: any) => {
   const videoManagerMachine = setup({
     types: {
-      context: {} as { videos: ListVideosOutput },
+      context: {} as { videos: ListVideosOutput; errorMessage: string | null },
       events: {} as { type: "INIT" },
     },
     actors: {
@@ -28,6 +28,7 @@ export const getVideoManagmentMachine = (fetch: any) => {
     /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOgEkARAGQFEBiMgOTIBUBtABgF1FQAHAPaxcAF1wD8vEAA9EAVjkAmEnIAca1QBZFigIwcAnAGZNAGhABPRLsWqSNgOwA2Xc7kcOzzZoC+P82hYeISkAGI0LADCABJMAOIA+lRkAMosCQBqlDQA8il0EBJgJAQAbgIA1sWBOATEJOFRsYyJyWmZ2XkIZQKY6GISnFxDUoLCA5JIMogG7iqaunIGuitGelrmVggrTiSKmqqKBqocqov7Dn4BGLUhDREx8Ump6VkUuflgAE5fAl8kfAANv0AGZ-VAkGrBeqNR4tZ7tN4fbr4cp9CZDEZTMaicSTUCyBAKZRqDTaPSGEybRBnEhrI6KNSGVQOXROAxXEBQuphB7NVovDrvPIJUIAQTItAodGksBE-WK6BBIm+yH0RDo3LusP5CNenRSoolUqx-CEuIkUkJRiMchITk0ak0BicNu02mpCEZRjpbO07iMDgciicHEUnK1ML5Tza+uFhpSAFVIpEaCkUqFE1QZXKFSQlSqvmqOBrI7ymjHBUiRUmU2mM1nTSAcRMrTNjCQHEY-aonF3Zg4DJ7GXZVMcON4HX3vOHOfgBBA4FIy6Nza2poSALROT2bu0GA+Ho+H3sRm7Q0iUWir8Z4tsITQuEgcfRGB02BRaEPDuS6EgGfsu00CcOBcFkzyCHl7greFYyFD4bwtfFpgQVkDE7A9gNOXRbWdIdLBmP93FOIMX10ADDiMCDbijGCBURA0jUlGgKEQ9cCUQD0CK9Gx7XUE5QIcJQDGdaiL2guF6LjD4ElrVN00zKg2LvDdrBWdDDCcfYDldVRcOHXQ7BDftjiWVQ9MuPwfCAA */
     context: {
       videos: [],
+      errorMessage: null,
     },
     initial: "IDLE",
     states: {
@@ -50,19 +51,14 @@ export const getVideoManagmentMachine = (fetch: any) => {
         },
       },
       FETCHING_LIST_VIDEOS_FAILED: {
-        entry: (data: any) =>
-          console.error(`Failed Because ` + data.event.error),
-        after: {
-          10: "IDLE",
-        },
+        entry: assign({ errorMessage: (data: any) => data.event.error }),
+        type: "final",
       },
       FETCHING_LIST_VIDEOS_SUCCESSFUL: {
         entry: assign({
           videos: (data: any) => data.event.output,
         }),
-        after: {
-          10: "IDLE",
-        },
+        type: "final",
       },
     },
   });
