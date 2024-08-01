e8fc4da172dfbd2bb4bcdbdad66b46086ad0c71b
piyush arora <piyusharorawork@gmail.com>
2024-08-01 20:05:51 +0530

    add frame number to video background

--- apps/site/src/pages/Playground.tsx
+++ apps/site/src/pages/Playground.tsx
@@ -1,12 +1,20 @@
 import { VideoEditor } from "@via/ui/video-editor";
+import { useState } from "react";
 
 export default function Playground() {
+  const [recording, setRecording] = useState(false);
+
   return (
     <section className="flex flex-col h-screen items-center">
       <header>Playground</header>
       <div className="mobile bg-black my-4 rounded-xl">
-        <VideoEditor />
+        <VideoEditor recording={recording} onFinish={() => alert("done")} />
       </div>
+      <footer className="flex">
+        <button className="btn btn-primary" onClick={() => setRecording(true)}>
+          Record
+        </button>
+      </footer>
     </section>
   );
 }
--- packages/ui/src/video-editor.tsx
+++ packages/ui/src/video-editor.tsx
@@ -1,10 +1,34 @@
 import { Canvas } from "@react-three/fiber";
 import { Text, useVideoTexture } from "@react-three/drei";
+import { useEffect, useState } from "react";
 
-const VideoBackground = () => {
-  const videoTexture: any = useVideoTexture(
+let pause: any;
+const FPS = 60; // TODO need to get it from backend
+const MAX_FRAMES = 300;
+const WAIT_MS = 100;
+
+type VideoBackgroundProps = {
+  frame: number;
+};
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const VideoBackground = (props: VideoBackgroundProps) => {
+  const videoTexture = useVideoTexture(
     "http://localhost:4000/uploads/1722237708740.mp4"
   );
+
+  useEffect(() => {
+    const videoElement: HTMLVideoElement = videoTexture.source.data;
+    if (!videoElement) {
+      return;
+    }
+
+    videoElement.pause();
+    videoElement.currentTime = props.frame / FPS;
+  }, [props.frame]);
 
   return (
     <mesh>
@@ -14,7 +38,30 @@ const VideoBackground = () => {
   );
 };
 
-export const VideoEditor = () => {
+type VideoEditorProps = {
+  recording: boolean;
+  onFinish: () => void;
+};
+
+export const VideoEditor = (props: VideoEditorProps) => {
+  const [frame, setFrame] = useState(0);
+
+  const startRecording = async () => {
+    for (let i = 0; i < MAX_FRAMES; i++) {
+      setFrame((frame) => frame + 1);
+      await sleep(100);
+    }
+    props.onFinish();
+  };
+
+  useEffect(() => {
+    if (!props.recording) {
+      return;
+    }
+
+    startRecording();
+  }, [props.recording]);
+
   return (
     <Canvas>
       <Text
@@ -26,7 +73,7 @@ export const VideoEditor = () => {
       >
         Hello
       </Text>
-      <VideoBackground />
+      <VideoBackground frame={frame} />
     </Canvas>
   );
 };
