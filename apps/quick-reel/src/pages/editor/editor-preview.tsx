622635b911e6f154b500918b15182ab6aea703de
piyush arora <piyusharorawork@gmail.com>
2025-01-14 12:31:04 +0530

    use seek for editor preview

--- apps/quick-reel/src/pages/editor/editor-preview.tsx
+++ apps/quick-reel/src/pages/editor/editor-preview.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
   frameNumber: number;
@@ -7,10 +7,12 @@ type Props = {
 
 export const EditorPreview = (props: Props) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [seeking, setSeeking] = useState(false);
 
   useEffect(() => {
     const video = videoRef.current;
     if (!video) return;
+    if (seeking) return;
 
     video.currentTime = props.frameNumber / props.fps;
   }, [props.frameNumber, videoRef.current, props.fps]);
@@ -21,6 +23,8 @@ export const EditorPreview = (props: Props) => {
         ref={videoRef}
         className="aspect-[9/16] w-[335px] "
         crossOrigin="anonymous"
+        onSeeking={() => setSeeking(true)}
+        onSeeked={() => setSeeking(false)}
       >
         <source
           src="https://res.cloudinary.com/dmtxtgvsb/video/upload/v1736600178/hotel-highlight-reel-original.mp4"
