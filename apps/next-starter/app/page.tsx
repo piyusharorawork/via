850364b2c4730888b0f9434e51a4c8b69ebe4c1e
piyush arora <piyusharorawork@gmail.com>
2025-01-19 20:12:57 +0530

    next starter multiple pages

--- apps/next-starter/app/page.tsx
+++ apps/next-starter/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-center">Home</h1>
-      <Button>Click Me</Button>
+      <div className="flex gap-2 items-center">
+        <Button>Click Me</Button>
+        <Link href="/about">About</Link>
+      </div>
     </>
   );
 }
