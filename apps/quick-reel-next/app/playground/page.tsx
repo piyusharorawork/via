10d2e02f76a33b7bd745bf149c9f89ae496c0775
piyush arora <piyusharorawork@gmail.com>
2025-01-28 15:22:17 +0530

    add playground for video preview

--- apps/quick-reel-next/app/playground/page.tsx
+++ apps/quick-reel-next/app/playground/page.tsx
@@ -1,10 +1,10 @@
-import { Preview } from "./preview";
+import { VideoPreview } from "./video-preview";
 
 export default function PlaygroundPage() {
   return (
     <section className="h-screen w-full flex flex-col">
       <h1 className="text-center">Playground</h1>
-      <Preview />
+      <VideoPreview />
     </section>
   );
 }
