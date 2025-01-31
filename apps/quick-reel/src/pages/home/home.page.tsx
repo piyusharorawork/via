f1be0d22609aa63bad2562d978d3d0fe513efd69
piyush arora <piyusharorawork@gmail.com>
2025-01-31 22:15:48 +0530

    removed quick reel

--- apps/quick-reel/src/pages/home/home.page.tsx
+++
@@ -1,8 +0,0 @@
-import { useStore } from "@/store/store";
-import { VideoTemplates } from "./video-templates";
-
-export default function HomePage() {
-  const videoTemplates = useStore((state) => state.videoTemplates);
-
-  return <VideoTemplates templates={videoTemplates} />;
-}
