9eb908443795a3540dd22cea47d807fe9186e07a
piyush arora <piyusharorawork@gmail.com>
2025-02-01 12:29:40 +0530

    expo starter init

---
+++ starters/expo/README.md
@@ -0,0 +1,1 @@
+# Expo Starter
---
+++ starters/expo/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function AppPage() {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
