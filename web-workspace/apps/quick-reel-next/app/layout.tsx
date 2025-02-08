f8e736936cc264c2d61c9202b2a993e59703edf0
piyush arora <piyusharorawork@gmail.com>
2025-02-09 01:14:13 +0530

    with router working

--- web-workspace/apps/quick-reel-next/app/layout.tsx
+++ web-workspace/apps/quick-reel-next/app/layout.tsx
@@ -14,9 +14,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="//unpkg.com/react-scan/dist/auto.global.js" />
+        {/* <script src="//unpkg.com/react-scan/dist/auto.global.js" /> */}
       </head>
-      <body>{children}</body>
+      <body
+        style={
+          {
+            // overflow: "hidden",
+            // position: "fixed",
+            // width: "100%",
+          }
+        }
+      >
+        {children}
+      </body>
     </html>
   );
 }
